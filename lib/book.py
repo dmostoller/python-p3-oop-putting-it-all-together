c93984dd0f65cab7ffdc29b69ebb3ff8354bc84d
David Mostoller <dmostoller@gmail.com>
2024-01-31 02:23:06 -0500

    completed lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,41 @@
 #!/usr/bin/env python3
 
-class Book:
-    pass
+class Book():
+    
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, new_title):
+        self._title = new_title
+
+
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, new_page_count):
+        if type(new_page_count) == int:
+            self._page_count = new_page_count
+        else:
+            raise Exception("page_count must be an integer")
+    
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+book = Book("And Then There Were None", 25)
+
+book.turn_page()
+
+print(book.page_count)
+print(book.title)
+
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,39 @@
 #!/usr/bin/env python3
 
-class Shoe:
-    pass
+class Shoe():
+    
+    def __init__(self, brand, size, condition = "Used"):
+        self.brand = brand
+        self.size = size
+        self.condition = condition
+
+
+    @property
+    def brand(self):
+        return self._brand 
+    
+    @brand.setter
+    def brand(self, new_brand):
+        self._brand = new_brand
+
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, new_size):
+        if type(new_size) == int:            
+            self._size = new_size
+        else:
+            raise Exception("size must be an integer")
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your show is as good as new")
+
+stan_smith = Shoe("Adidas", 9)
+
+print(stan_smith.size)
+print(stan_smith.brand)
+stan_smith.cobble()
+print(stan_smith.condition)
